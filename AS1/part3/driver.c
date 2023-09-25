45b6aea95e3f23570fb71b3db9515e837c92c7b2
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-09-24 18:39:18 -0700

    updated part3 driver

--- AS1/part3/driver.c
+++ AS1/part3/driver.c
@@ -9,11 +9,11 @@ int main(void) {
     Image *boat = load_image("boat.pgm");
     Image *f16 = load_image("f_16.pgm");
 
-    equalize_contrast(boat);
-    equalize_contrast(f16);
+    Image *boat_eq = image_contrast_eq(boat);
+    Image *f16_eq = image_contrast_eq(f16);
 
-    write_image("boat_eq.pgm", boat);
-    write_image("f16_eq.pgm", f16);
+    write_image("boat_eq.pgm", boat_eq);
+    write_image("f16_eq.pgm", f16_eq);
     
     return 0;
 }
