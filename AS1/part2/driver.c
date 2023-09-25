f512d74407db5afdef98ec219ec43bdce357fec0
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-09-24 18:38:57 -0700

    updated part2 driver

--- AS1/part2/driver.c
+++ AS1/part2/driver.c
@@ -5,12 +5,17 @@
 #include "image.h"
 #include "util.h"
 
+/* ~experimental~ */
 int main(void) {
-    Image *lenna = load_image("peppers.pgm");
+    Image *lenna = load_image("lenna.pgm");
 
-    image_requantize(lenna, 64);
+    image_contrast_eq(lenna);
 
-    write_image("lenna_rq.pgm", lenna);
+    Image *lenna_rq_inv = image_requantize(lenna, 32, 1);
+    Image *lenna_rq = image_requantize(lenna, 32, 0);
+
+    write_image("lenna_rq_inv.pgm", lenna_rq_inv);
+    write_image("lenna_rq.pgm", lenna_rq);
     
     return 0;
 }
