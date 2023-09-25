9b185f00eb174b9d3649086fb38e222e8361392c
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-09-24 20:21:53 -0700

    updated test driver

--- AS1/test/driver.c
+++ AS1/test/driver.c
@@ -8,7 +8,7 @@
 
 int main(void) {
     int dwidth = 10;
-    int wwidth = 5;
+    int wwidth = 3;
 
     int dsize = dwidth * dwidth;
     int wsize = wwidth * wwidth;
@@ -17,25 +17,19 @@ int main(void) {
     uint16_t *window = NULL;
 
     srand(time(NULL));
-    data[0] = rand() % UINT8_MAX;
-    printf(" %d", data[0]);
-    for (int i = 1; i < dsize; i += 1) {
-        if (i % dwidth == 0) printf("\n");
-        data[i] = ~data[i - 1] + 1;   // causes collisions!
+    for (int i = 0; i < dsize; i += 1) {
+        data[i] = (uint16_t)rand() % UINT8_MAX;
+        if (i % dwidth == 0) printf("\n"); 
         printf(" %d", data[i]);
     }
     printf("\n\n");
 
 
-    Window *w = new_window(wwidth, 0, wsize);
+    Window *w = new_window_sq(wwidth, 8);
     window = read_window(data, dsize, dwidth, w);
 
-    for (int i = 0; i < wsize; i += 1) {
-        if (!(window[i] + window[(i + wsize) % wsize])) {
-            printf(" oob");
-        } else {
-            printf(" %d", window[i]);
-        }
+    for (int i = 0; i < w->len; i += 1) {
+        printf(" %d", window[i]);
     }
     printf("\n");
 
