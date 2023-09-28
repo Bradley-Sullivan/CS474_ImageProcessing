1b8182c92965f0f20358c58b0b8e236f69383a25
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-09-27 19:59:57 -0700

    tweaked requantization driver

--- AS1/part2/driver.c
+++ AS1/part2/driver.c
@@ -9,12 +9,8 @@
 int main(void) {
     Image *lenna = load_image("lenna.pgm");
 
-    image_contrast_eq(lenna);
+    Image *lenna_rq = image_requantize(lenna, 2, 0);
 
-    Image *lenna_rq_inv = image_requantize(lenna, 32, 1);
-    Image *lenna_rq = image_requantize(lenna, 32, 0);
-
-    write_image("lenna_rq_inv.pgm", lenna_rq_inv);
     write_image("lenna_rq.pgm", lenna_rq);
     
     return 0;
