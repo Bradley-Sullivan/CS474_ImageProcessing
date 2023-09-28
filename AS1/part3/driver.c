b241e398e13ba7895902515ea5a2d9284740873d
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-09-27 20:00:14 -0700

    tweaked part3 driver

--- AS1/part3/driver.c
+++ AS1/part3/driver.c
@@ -9,8 +9,8 @@ int main(void) {
     Image *boat = load_image("boat.pgm");
     Image *f16 = load_image("f_16.pgm");
 
-    Image *boat_eq = image_contrast_eq(boat);
-    Image *f16_eq = image_contrast_eq(f16);
+    Image *boat_eq = image_hist_eq(boat);
+    Image *f16_eq = image_hist_eq(f16);
 
     write_image("boat_eq.pgm", boat_eq);
     write_image("f16_eq.pgm", f16_eq);
