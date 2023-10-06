c6f44602967c25e769fd2f4674fed02ec370b0c0
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-10-06 16:13:25 -0700

    added mask distinction

--- inc/image.h
+++ inc/image.h
@@ -15,13 +15,23 @@ typedef struct Image {
     uint8_t *data;
 } Image;
 
+typedef struct Mask {
+    uint16_t m, n;
+    size_t size, sum;
+    float *data;
+} Mask;
+
 Image *new_image(int m, int n, int q);
 Image *copy_image(Image* img);
 int del_image(Image *img);
 Image *load_image(const char *fname);
 int load_header(FILE *fp, Image *img);
 int load_data(FILE *fp, Image *img);
-
 int write_image(const char *fname, Image *img);
+
+Mask *new_mask(int m, int n);
+Mask *copy_mask(Mask *msk);
+Mask *mask_image(const char *fname);
+int del_mask(Mask *msk);
 
 #endif // IMAGE_H
