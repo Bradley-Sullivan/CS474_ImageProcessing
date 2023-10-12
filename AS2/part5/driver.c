06eaa1eb944f0b8c9910bf36fbf805ddb7c2e9ff
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-10-12 14:51:41 -0700

    removed extraneous images

--- AS2/part5/driver.c
+++ AS2/part5/driver.c
@@ -8,26 +8,18 @@
 
 int main(void) {
     Image *input = load_image("catstronaut.pgm");
-    Image *input_dx = new_image(input->m, input->n, input->q);
-    Image *input_dy = new_image(input->m, input->n, input->q);
+    Image *sobel_grad = new_image(input->m, input->n, input->q);
+    Image *prewitt_grad = new_image(input->m, input->n, input->q);
 
-    image_gradient(input, input_dx, input_dy, PREWITT);
+    prewitt_grad = image_gradient(input, PREWITT);
 
-    Image *psum = image_add(input_dx, input_dy);
+    sobel_grad = image_gradient(input, SOBEL);
 
-    write_image("prewitt_dx.pgm", input_dx);
-    write_image("prewitt_dy.pgm", input_dy);
-    write_image("prewitt_sum.pgm", psum);
+    write_image("sobel_grad.pgm", sobel_grad);
+    
+    write_image("prewitt_grad.pgm", prewitt_grad);
 
-    image_gradient(input, input_dx, input_dy, SOBEL);
-
-    Image *ssum = image_add(input_dx, input_dy);
-
-    write_image("sobel_dx.pgm", input_dx);
-    write_image("sobel_dy.pgm", input_dy);
-    write_image("sobel_sum.pgm", ssum);
-
-    del_image(input); del_image(input_dx); del_image(input_dy);
+    del_image(input); del_image(sobel_grad); del_image(prewitt_grad);
 
     return 0;
 }
