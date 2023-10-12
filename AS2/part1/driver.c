d9042492f6772a35382f264d7f7ea6dabb9170e9
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-10-12 14:47:26 -0700

    revised part 1

--- AS2/part1/driver.c
+++ AS2/part1/driver.c
@@ -3,21 +3,25 @@
 #include <time.h>
 #include <math.h>
 #include <inttypes.h>
+#include <getopt.h>
 
 #include "util.h"
 
-int main(void) {
-    Image *lenna = load_image("Image.pgm");
-    Mask *mask = mask_image("Pattern.pgm");
+#define NUM_ARGS 2
+
+int main(int argc, char *argv[]) {
+    if (argc - 1 < NUM_ARGS || argc - 1 > NUM_ARGS) {
+        fprintf(stderr, "Correct Usage:\n\t%s [input] [mask]\n\n\t[input] --> filename of input image\n\t[mask] --> filename of mask image\n", argv[0]);
+        return 1;
+    }
+
+    Image *lenna = load_image(argv[1]);
+
+    Mask *mask = mask_image(argv[2]);
 
     Image *out = image_correlate(lenna, mask);
 
-    write_image("corr_test.pgm", image_hist_eq(out));
-
-    Image *sum = image_add(lenna, image_hist_eq(out));
-
-    write_image("corr_test_add.pgm", sum);
-    write_image("corr_test.pgm", image_hist_eq(out));
+    write_image("correlation.pgm", out);
 
     del_image(lenna); del_mask(mask); del_image(out);
 
