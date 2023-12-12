191c94e3460aced4a9977b386633fbe3421d8432
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-12-11 18:05:09 -0800

    comments!

--- AS4/part2/driver.c
+++ AS4/part2/driver.c
@@ -90,49 +90,62 @@ int main(int argc, char* argv[]) {
 
 Image *freq_filter(Image *img, double filter[][4], int fr, int fc) {
     Image *ret = copy_image(img);
+    // creates mask/filter with power of two dimensions for FFT
     Mask *mask = new_mask(is_two_power(img->m) ? img->m : nx_power_two(img->m),   \
                            is_two_power(img->n) ? img->n : nx_power_two(img->n));
-    int r = (mask->m / 2) - ((fr + 1) / 2);
-    int c = (mask->n / 2) - ((fc + 1) / 2);
+    int r = (mask->m / 2) - ((fr + 1) / 2);     // centering row offset
+    int c = (mask->n / 2) - ((fc + 1) / 2);     // centering col offset
     double th, **md, **id;
     double p[2];
     uint16_t x, y;
 
+    // insert filter into center of mask
     for (int i = r; i < r + fr + 1; i += 1) {
         for (int k = c; k < c + fc + 1; k += 1) {
             mask->data[i * mask->n + k] = filter[i - r][k - c];
         }
     }
 
+    // obtain complex-data array for mask and image
     md = mask_complex(mask, 1);
     id = image_complex(ret, 1);
 
+    // take mask and image into the frequency domain
     dft2D(id, ret->m, ret->n, -1);
     dft2D(md, mask->m, mask->n, -1);
 
+    // visualize mask's DFT (works for sobel masks, buggy for laplacian)
     visualize_filter(md, mask->m, mask->n);
 
+    // iterate image complex-data array
     for (uint16_t i = 0; i < ret->m; i += 1) {
         for (uint16_t k = 1; k < 2 * ret->n; k += 2) {
-            x = ((k - 1) >> 1); y = i;
-            id[i][k] *= pow(-1,x+y);
-            id[i][k + 1] *= pow(-1,x+y);
+            x = ((k - 1) >> 1); y = i;          // compute x,y coord for centering
+            // placing the filter into the center of the mask -> multiply by pow(-1,x+y)
+            // need to undo it to match up with our image's centered DFT (kinda confusing i know)
+            md[i][k] *= pow(-1,x+y);
+            md[i][k + 1] *= pow(-1,x+y);
+            // apply our mask to our image
             cmult(md[i] + k, id[i] + k, p);
+            // store the resultant product
             id[i][k] = p[0];
             id[i][k + 1] = p[1];
         }
     }
 
+    // bring our image back into the spatial domain
     dft2D(id, ret->m, ret->n, 1);
 
     for (uint16_t i = 0; i < ret->m; i += 1) {
         for (uint16_t k = 1; k < ret->n * 2; k += 2) {
             x = ((k - 1) >> 1); y = i;
+            // scales and stores resultant real-component of our image's filtered DFT
             th = CLAMP(0,UINT8_MAX, 1000 * log(1 + (id[i][k] * pow(-1,x+y))));
             ret->data[y * ret->n + x] = th;
         }
     }
 
+    // free our mask
     del_mask(mask);
 
     return ret;
@@ -146,6 +159,8 @@ void visualize_filter(double **d, int m, int n) {
         for (uint16_t k = 1; k < n * 2; k += 2) {  // ...
             x = ((k - 1) >> 1); y = i;
             im = d[i][k + 1] * pow(-1,x+y);
+            // need to be able to visualize negative values (sobel mask)
+            // maps complex-component into a "signed" range of UINT8_MAX
             th = 128 - (im * 10000);
             vis->data[i * n + ((k - 1) >> 1)] = CLAMP(0,UINT8_MAX,th);
         }
